ffd63dad3b6189400704e6f349f4b1a28e7b8192
Vedangmodi <modivedang.mv94@gmail.com>
2026-05-02 14:59:24 +0530

    Add prod properties + postgres migration + groq AI

--- src/main/java/com/vedang/jobpilot_ai/JobpilotAiApplication.java
+++ src/main/java/com/vedang/jobpilot_ai/JobpilotAiApplication.java
@@ -2,8 +2,10 @@ package com.vedang.jobpilot_ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class JobpilotAiApplication {
 
 	public static void main(String[] args) {
