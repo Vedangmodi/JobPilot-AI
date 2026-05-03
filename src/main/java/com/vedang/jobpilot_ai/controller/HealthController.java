4b26abbfb70bfc8ff6893d868447b3d6c6ea9cdc
Vedangmodi <modivedang.mv94@gmail.com>
2026-05-03 13:30:32 +0530

    Add health check

---
+++ src/main/java/com/vedang/jobpilot_ai/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.vedang.jobpilot_ai.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> health(){
+        return ResponseEntity.ok("JobPilot-AI is up and running! 🚀");
+
+    }
+}
